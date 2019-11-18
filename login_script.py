52596ca80ef25daa0a5bfd050f180888005d908f
zx <zhouxieq@126.com>
2019-11-18 18:56:58 +0800

    Add files via upload

---
+++ login_script.py
@@ -0,0 +1,79 @@
+﻿import os
+import time
+
+import cv2
+import numpy as np
+from PIL import ImageGrab
+from pykeyboard import PyKeyboard
+from pymouse import PyMouse
+import win32api, win32con
+
+k = PyKeyboard()
+m = PyMouse()
+
+
+# 匹配图片相似度
+def dingwei(sh_img, tar_img):
+    # sh_img 截图 tar_img 辨识图
+    sh_img_data = cv2.imread(sh_img, 0)
+    tar_img_data = cv2.imread(tar_img, 0)
+
+    threshold = 0.95  # 匹配度
+    gps = []
+    res = cv2.matchTemplate(sh_img_data, tar_img_data, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(res >= threshold)    
+    for pt in zip(*loc[::-1]):
+        gps.append(pt)
+    #print(gps)
+    return gps
+
+
+# 截图检测登陆界面
+def screenshot_login():
+    bbox = (0, 0, 1000, 500)
+    im = ImageGrab.grab(bbox)
+    im.save('login.png')
+    return 'login.png'
+
+
+# 截图战网登陆按钮
+def screenshot_bnet():
+    bbox = (0, 0, win32api.GetSystemMetrics(win32con.SM_CXSCREEN), win32api.GetSystemMetrics(win32con.SM_CYSCREEN))
+    im = ImageGrab.grab(bbox)
+    im.save('bnet.png')
+    return 'bnet.png'
+
+
+def start_check():
+    login_img = screenshot_login()
+    gps = dingwei(login_img, '1.png')
+    os.remove(login_img)
+    k.tap_key(k.space_key) 
+    if len(gps) > 0:
+        # 切换
+        print('检测登陆界面，匹配成功')
+        k.press_key(k.alt_key)
+        k.tap_key(k.function_keys[4])
+        k.release_key(k.alt_key)
+
+        k.press_key(k.alt_key)
+        k.tap_key(k.tab_key)
+        k.release_key(k.alt_key)
+        print('切换')
+        time.sleep(1)
+        bnet_img = screenshot_bnet()
+        gps = dingwei(bnet_img, '2.png')
+        os.remove(bnet_img)
+        if len(gps) > 0:
+            print('检测战网登陆界面，匹配成功')
+            for (x, y) in gps:
+                m.click(int(x) + 50, int(y) + 50, 1)
+                print('点击登陆')
+                time.sleep(30)
+                k.tap_key(k.enter_key)         
+
+
+if __name__ == '__main__':
+    while True:
+        start_check()
+        time.sleep(10)
---
+++ setup.bat
@@ -0,0 +1,37 @@
+@echo off
+
+
+echo 安装环境中...
+echo.
+
+
+echo 安装opencv...
+pip install opencv_python-4.1.1.26-cp37-cp37m-win32.whl
+echo.
+
+echo 安装PIL...
+pip install Pillow-6.2.1-cp37-cp37m-win32.whl
+echo.
+
+echo 安装pywin32...
+pip install pywin32-227-cp37-cp37m-win32.whl
+echo.
+
+echo 安装pyHook...
+pip install pyHook-1.5.1-cp37-cp37m-win32.whl
+echo.
+
+echo 安装numpy...
+pip install numpy-1.17.4-cp37-cp37m-win32.whl
+echo.
+
+echo 安装PyUserInput...
+pip install PyUserInput-0.1.11-py2-none-any.whl
+echo.
+
+cls
+
+echo 环境安装完成，请启动start.bat开始挂机...
+echo.
+
+pause
